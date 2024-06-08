8d7f5a47819b5c657a6e416bb5faee71c5cd1f29
kasahun-welela <kasahunwelela1@gmail.com>
2024-06-08 14:23:06 +0300

    install remix icon

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
+import "remixicon/fonts/remixicon.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
