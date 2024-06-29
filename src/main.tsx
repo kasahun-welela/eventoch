af13a481b92de740096d7d170693b05d466a05f2
kasahun-welela <kasahunwelela1@gmail.com>
2024-06-30 01:12:11 +0300

    add login and 404 routing

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import "remixicon/fonts/remixicon.css";
+import Login from "./components/auth/Login.tsx";
+import NotFound from "./components/NotFound.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -12,7 +14,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
         </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
